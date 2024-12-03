e64af596b8827a1698f7d3b3c37f2bbc252aed1d
Mansi agarwal <mansiagarwal293@gmail.com>
2024-12-03 22:27:19 +0530

    login function

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-function signup(username) {
-  let user = ["Mansi", "Aditi", "Chhaya", "Neha", "Ritik"];
-  if (user.includes(username)) {
+function signup(userName) {
+  let usernames = ["Mansi", "Aditi", "Chhaya", "Neha", "Ritik"];
+  if (usernames.includes(userName)) {
     return "Already Registered, Please Login";
   } else {
-    user.push(username);
+    usernames.push(userName);
     return "Signup Sucessfull,Please Login";
   }
 }
 console.log(signup("Mansi"));
+
+function login(userName, password) {
+  let usernames = ["Mansi", "Aditi", "Chhaya", "Neha", "Ritik"];
+  let pass = "Emp@123";
+  if (usernames.includes(userName) && password === pass) {
+    return "Login Sucessful";
+  } else if (usernames.includes(userName) && password !== pass) {
+    return "Wrong Password";
+  } else {
+    return "user not found , Please signup";
+  }
+}
+console.log(login("Mans7", "Emp@124"));
